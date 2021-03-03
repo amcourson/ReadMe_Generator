2e5a320fc9a35f4bb26aac8bcbe552f71e19a8ec
amcourson <amcourson09@sbcglobal.net>
2021-03-02 19:47:10 -0600

    updating index.js

---
+++ generateMarkdown.js
@@ -0,0 +1,29 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.name}
+## description
+${data.description}
+## install
+${data.install}
+## usage
+${data.usage}
+## contribution
+${data.contribution}
+## test
+${data.test}
+`;
+}
+
+module.exports = generateMarkdown;
---
+++ index.js
@@ -0,0 +1,64 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
+const fs = require('fs')
+// TODO: Create an array of questions for user input
+const questions = [ 
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'What is the description of your project?',
+  },
+  {
+    type: 'input',
+    name: 'install',
+    message: 'What are the install instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What are the usage requirements?',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'What are the contribution guidelines?',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What are the test instructions?',
+  },
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data)
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt
+    (questions)
+      .then(answers => {
+       var readMeText = generateMarkdown(answers)
+       console.log(readMeText)
+    writeToFile('./Output/ReadMe.md',readMeText)
+      })
+      .catch(error => {
+        if(error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          // Something else went wrong
+        }
+      });
+}
+
+// Function call to initialize app
+init();
